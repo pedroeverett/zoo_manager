8b322017196d74cdf69e1f98f79e30ae23de1b20
Pedro Everett <pedrojoseeverett@gmail.com>
2017-07-09 10:54:31 +0100

    GeneralEnclosure class and methods created

---
+++ zoo_management/GeneralEnclosure.java
@@ -0,0 +1,71 @@
+package zoo_management;
+import behaviours.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GeneralEnclosure extends Enclosure {
+
+  private ArrayList<Animal> cage;
+
+  public GeneralEnclosure (String name, EnclosureType type, double size) {
+    super(name, type, size);
+    this.name = name;
+    this.type = type;
+    this.size = size;
+    this.cage = new ArrayList<Animal>();
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public EnclosureType getType() {
+    return this.type;
+  }
+
+  public double getSize() {
+    return this.size;
+  }
+
+  public int getNumberAnimalsInCage() {
+    return this.cage.size();
+  }
+
+  public void addAnimal(Animal animal) {
+    double animalSize = animal.getSize();
+    if (animalSize <= this.size) {
+      this.cage.add(animal);
+      this.size -= animalSize;
+    }
+  }
+
+  public int removeAnimalByName(Animal animal) {
+    this.cage.remove(animal);
+    return getNumberAnimalsInCage();
+  }
+
+  public int removeAnimalByIndex(int index) {
+    this.cage.remove(index);
+    return getNumberAnimalsInCage();
+  }
+
+  public void addAnimalIfTheyWontEatEachOther(Animal newAnimal) {
+    double animalSize = newAnimal.getSize();
+    for (Animal animal : cage)
+    if (animalSize <= this.size && animal.getDangerLevel() == newAnimal.getDangerLevel()) {
+      this.cage.add(newAnimal);
+      this.size -= animalSize;
+    }
+  }
+
+  public int randomAnimalRampage() {
+      Random ramp = new Random();
+      int cageSize = this.getNumberAnimalsInCage();
+      int index = ramp.nextInt(cageSize);
+      removeAnimalByIndex(index);
+      // String name = index.getName();
+      // return name;
+      return getNumberAnimalsInCage();
+  }
+
+}
